99c4effc27c124806dfe32312a30ad4bda75f029
Mayowa <mayowa59121@gmail.com>
2023-12-03 18:54:08 +0000

    Initial commit

---
+++ nursepal_backend/nursepal/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import user_login, user_logout
+
+
+urlpatterns = [
+    path('api/login', user_login, name='user_login'),
+    path('api/logout', user_logout, name='user_logout'),
+]
---
+++ nursepal_backend/nursepal/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+import json
+from django.db import connection
+
+# Create your views here.
+
+
+@api_view(['POST'])
+def user_login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = User.objects.get(username=username)
+
+    try:
+        user = User.objects.get(username=username)
+        if user.check_password(password):
+            print("Executed Queries:", connection.queries)
+            return JsonResponse({'message': 'Login successful'}, status=200)
+        else:
+            return JsonResponse({'message': 'Login failed'}, status=401)
+    except User.DoesNotExist:
+        return JsonResponse({'message': 'User does not exists'}, status=401)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return JsonResponse({'message': e}, status=401)
+
+
+@api_view(['POST'])
+def user_logout(request):
+    return JsonResponse({'message': 'Logout successful'})
